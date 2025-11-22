856ea96bf6a3d0c8498a23f26db0528437b23ff0
Jeremy Klein <hellojeremysherry@gmail.com>
2025-11-22 01:07:11 -0800

    CHANGES

--- printscalendar/urls.py
+++ printscalendar/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from calendarapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('calendarapp.urls')),
+    path("api/scrape-auction/", views.scrape_auction, name="scrape_auction"),
+
 ]
