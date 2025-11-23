76567361f1d22bcd16e343299455b090b9a149c4
Jeremy Klein <hellojeremysherry@gmail.com>
2025-11-22 21:01:49 -0800

    CHANGES

--- calendarapp/forms.py
+++ calendarapp/forms.py
@@ -1,7 +1,41 @@
 from django import forms
 from .models import SaleEvent
 
+
 class SaleEventForm(forms.ModelForm):
+    # Make start_date more flexible
+    start_date = forms.DateField(
+        label="Start date",
+        input_formats=[
+            "%Y-%m-%d",  # 2025-10-25
+            "%m/%d/%Y",  # 10/25/2025
+            "%m/%d/%y",  # 10/25/25
+            "%m-%d-%Y",  # 10-25-2025
+        ],
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "e.g. 10/25/2025 or 2025-10-25",
+            }
+        ),
+    )
+
+    # Same flexibility for end_date (optional)
+    end_date = forms.DateField(
+        label="End date",
+        required=False,
+        input_formats=[
+            "%Y-%m-%d",
+            "%m/%d/%Y",
+            "%m/%d/%y",
+            "%m-%d-%Y",
+        ],
+        widget=forms.TextInput(
+            attrs={
+                "placeholder": "e.g. 10/26/2025 (optional)",
+            }
+        ),
+    )
+
     class Meta:
         model = SaleEvent
         fields = [
