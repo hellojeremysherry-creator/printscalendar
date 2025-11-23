158e2d1b128f97abc6d6b29fb73d74c8cb8f5ef4
Jeremy Klein <hellojeremysherry@gmail.com>
2025-11-22 20:25:33 -0800

    CHANGES

--- calendarapp/forms.py
+++ calendarapp/forms.py
@@ -2,12 +2,9 @@ from django import forms
 from .models import SaleEvent
 
 class SaleEventForm(forms.ModelForm):
-    auction_url = forms.URLField(required=False, label="Auction URL")
-
     class Meta:
         model = SaleEvent
         fields = [
-            "auction_url",       # new!
             "title",
             "auction_house",
             "location",
